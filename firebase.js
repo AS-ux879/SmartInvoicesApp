72c4665cffeff474df011637632c3eb9d009b464
Ali-ux <fno0o.m.505@gmail.com>
2025-12-02 18:25:11 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,30 @@
-// ==========================
-// firebase.js — FINAL
-// ==========================
+// firebase.js — FINAL FIXED VERSION
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
+import { 
+  getAuth 
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { 
+  getFirestore 
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import { 
+  getStorage 
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDEzLQRjRCn60WsUsY-aEFBKZ4Vy1iJceA",
   authDomain: "smartinvoicesapp-ebb2c.firebaseapp.com",
   projectId: "smartinvoicesapp-ebb2c",
-  storageBucket: "smartinvoicesapp-ebb2c.firebasestorage.app",
-  messagingSenderId: "578187855094",
-  appId: "1:578187855094:web:5e4414db92c5ad17afd8d7"
+  storageBucket: "smartinvoicesapp-ebb2c.appspot.com",
+  messagingSenderId: "665203531882",
+  appId: "1:665203531882:web:e793ac17dc0f5b5d92aa13",
+  measurementId: "G-YCB6R5HR28"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
